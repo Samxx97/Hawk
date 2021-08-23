5c14177ed47217ce5e59e30b22399323d28a67d7
Sam <samxxhaider@gmail.com>
2021-08-23 08:18:13 +0300

    Adds Core utility Header

--- Hawk/include/Hawk.h
+++ Hawk/include/Hawk.h
@@ -1,8 +1,11 @@
+#pragma once
 
 // to be used by Clients
 #include "core/Application.h"
-
+#include "core/Core.h"
 #include "core/log.h"
 
 //provides an entry point 
 #include "core/EntryPoint.h"
+
+
---
+++ Hawk/include/core/Core.h
@@ -0,0 +1,27 @@
+#pragma once
+
+namespace Hawk {
+
+#define BIT(X) (1 <<X )
+
+#define BIND_EVENT_FN(f, c) std::bind(&f, c, std::placeholders::_1)
+
+#define BIND_EVENT_FN_DEFAULT(f) BIND_EVENT_FN(f, this)
+
+	//TODO support other platforms such as unix etc.. 
+	//TODO Enable assertions in debug mode only
+#ifdef HK_ENABLE_ASSERTS
+#define HK_ASSERT(clause, ...) {if(!(clause)) {HK_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+#define HK_CORE_ASSERT(clause, ...) {if(!(clause)) {HK_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+
+#else
+#define HK_ASSERT(clause, ...)
+#define HK_CORE_ASSERT(clause, ...)
+
+#endif
+}
+
+
+
+
+
